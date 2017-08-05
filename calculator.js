7cc46625b2dde06e158a556cce72114266ea2081
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2017-08-05 18:45:15 -0500

    created files, began work, creating master

---
+++ calculator.js
@@ -0,0 +1,55 @@
+console.log("hey, hey");
+
+var number1 = document.getElementById("num1");
+var number2 = document.getElementById("num2");
+
+// put an event listener on each of the buttons
+document.addEventListener("click", calcMultiply);
+console.log(calcMultiply);
+
+
+// When the user performs one of the operations, 
+// output the result to another 
+// DOM element of your choice.
+
+
+/*
+  Create a function that multiplies two numbers
+  passed in as arguments. Return the product.
+ */
+function calcMultiply() {
+    HTMLOutputElement.innerHTML += num1 * num2;
+
+}
+calcMultiply();
+console.log(calcMultiply);
+
+
+
+/*
+  Create a function that adds two numbers
+  passed in as arguments. Return the sum.
+ */
+
+
+/*
+  Create a function that subtracts two numbers
+  passed in as arguments. Return the difference.
+ */
+
+
+/*
+  Create a function that divides two numbers
+  passed in as arguments. Return the quotient.
+ */
+
+
+
+/*
+  Create a function that accepts three arguments.
+    1. First number
+    2. Second number
+    3. A function that performs an operation on them
+
+  Return the value of the operation.
+ */
