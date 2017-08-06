17667ab7987aed0461db8e054216c370cf0f92af
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2017-08-05 22:38:48 -0500

    git

--- calculator.js
+++ calculator.js
@@ -1,55 +1,109 @@
 console.log("hey, hey");
 
-var number1 = document.getElementById("num1");
-var number2 = document.getElementById("num2");
-
-// put an event listener on each of the buttons
-document.addEventListener("click", calcMultiply);
-console.log(calcMultiply);
 
 
-// When the user performs one of the operations, 
-// output the result to another 
-// DOM element of your choice.
 
+//Addition:
+document.getElementById("addingbtn").onclick = function() { calculatorAdd() };
 
-/*
-  Create a function that multiplies two numbers
-  passed in as arguments. Return the product.
- */
-function calcMultiply() {
-    HTMLOutputElement.innerHTML += num1 * num2;
+function addNum(num1, num2) {
+    //get value input 1 from DOM
+    var num1 = document.getElementById("number1").value;
+    //get value of input 2 from DOM
+    var num2 = document.getElementById("number2").value;
+    //return the values parsed, to convert from string to number
+    return parseInt(num1) + parseInt(num2);
+}
+
+//the calculatorAdd function takes the performed math from the addNum and adds it to DOM
+function calculatorAdd() {
+    var sum = addNum(number1, number2);
+    document.getElementById("add").innerHTML = addNum(sum);
 
 }
-calcMultiply();
-console.log(calcMultiply);
+///////////////////////////////////////////////////////////////////
 
+//Subtraction:
+document.getElementById("subtractbtn").onclick = function() { calculatorSubtract() };
 
+function subNum(num1, num2) {
+    //get value input 1 from DOM
+    var num1 = document.getElementById("number1").value;
+    //get value of input 2 from DOM
+    var num2 = document.getElementById("number2").value;
+    //return the values parsed, to convert from string to number
+    return parseInt(num1) - parseInt(num2);
+}
 
-/*
-  Create a function that adds two numbers
-  passed in as arguments. Return the sum.
- */
+//the calculatorSubtract function takes the performed math from the subNum and adds it to DOM
+function calculatorSubtract() {
+    var minus = subNum(number1, number2);
+    document.getElementById("sub").innerHTML = subNum(minus);
 
+}
 
-/*
-  Create a function that subtracts two numbers
-  passed in as arguments. Return the difference.
- */
+///////////////////////////////////////////////////////////////////
 
+//Multiplication:
+document.getElementById("multiplybtn").onclick = function() { calculatorMultiply() };
 
-/*
-  Create a function that divides two numbers
-  passed in as arguments. Return the quotient.
- */
+function multNum(num1, num2) {
+    //get value input 1 from DOM
+    var num1 = document.getElementById("number1").value;
+    //get value of input 2 from DOM
+    var num2 = document.getElementById("number2").value;
+    //return the values parsed, with  * operator, to convert from string to number
+    return parseInt(num1) * parseInt(num2);
+}
 
+//the calculatorMultiply function takes the performed math from the multNum and adds it to DOM
+function calculatorMultiply() {
+    var multiply = multNum(number1, number2);
+    document.getElementById("sub").innerHTML = multNum(multiply);
 
+}
 
-/*
-  Create a function that accepts three arguments.
-    1. First number
-    2. Second number
-    3. A function that performs an operation on them
+///////////////////////////////////////////////////////////////////
 
-  Return the value of the operation.
- */
+//Division
+document.getElementById("dividebtn").onclick = function() { calculatorDivide() };
+
+function divNum(num1, num2) {
+    //get value input 1 from DOM
+    var num1 = document.getElementById("number1").value;
+    //get value of input 2 from DOM
+    var num2 = document.getElementById("number2").value;
+    //return the values parsed, with  * operator, to convert from string to number
+    return parseInt(num1) / parseInt(num2);
+}
+
+//the calculatorDivide function takes the performed math from the divNum and adds it to DOM
+function calculatorDivide() {
+    var divide = divNum(number1, number2);
+    document.getElementById("sub").innerHTML = divNum(divide);
+
+}
+
+//////////////////////////////////////////////////////////////////
+
+//3 argument function
+
+document.getElementById("doublebtn").onclick = function() { calculatorDouble() };
+
+function doubleNum(num1, num2, any) {
+
+    //get value input 1 from DOM
+    var num1 = document.getElementById("number1").value;
+    //get value of input 2 from DOM
+    var num2 = document.getElementById("number2").value;
+    //return the values parsed, with  * operator, to convert from string to number
+    return (parseInt(num1) * parseInt(num2)) * 2;
+
+};
+
+//the calculatorDivide function takes the performed math from the divNum and adds it to DOM
+function calculatorDouble() {
+    var double = doubleNum(number1, number2, doubleNum);
+    document.getElementById("double").innerHTML = doubleNum(double);
+
+}
